223134011a6c2879033e7e0171b6139838c5888f
Potassium-chromate <home7438072@gmail.com>
2025-05-15 05:36:57 +0800

    Initial commit

---
+++ Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,27 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/projdefs.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/portable.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/task.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/list.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/timers.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/task.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/event_groups.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/timers.h
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOS.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/projdefs.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/portable.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/task.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/list.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/timers.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/task.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/event_groups.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/timers.h:
---
+++ Debug/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,19 @@
+FreeRTOS/portable/MemMang/heap_4.o: ../FreeRTOS/portable/MemMang/heap_4.c \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/projdefs.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/portable.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/task.h \
+ C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/list.h
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOS.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/projdefs.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/portable.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/task.h:
+C:/Users/user/Desktop/stm_project/Lab5/FreeRTOS/include/list.h:
